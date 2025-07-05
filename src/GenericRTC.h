586ab3b12ca846942a7cfaedeae26dc40ec34740
JChristensen <jack.christensen@outlook.com>
2025-07-05 16:31:17 -0400

    Add temperature() method to the base class.

--- src/GenericRTC.h
+++ src/GenericRTC.h
@@ -4,7 +4,7 @@
 // Copyright (C) 2025 by Jack Christensen and licensed under
 // GNU GPL v3.0, https://www.gnu.org/licenses/gpl.html
 //
-// Allows a sketch to work with either type of RTC, determined at run time.
+// Allows a sketch to work with either type of RTC, can be determined at run time.
 
 #ifndef GENERIC_RTC_H_INCLUDED
 #define GENERIC_RTC_H_INCLUDED
@@ -20,10 +20,11 @@ class GenericRTC
         virtual void begin() = 0;
         virtual time_t get() = 0;
         virtual uint8_t set(const time_t t) = 0;
-        virtual uint8_t writeRTC(const uint8_t addr, const uint8_t* values, const uint8_t nBytes) =0;
+        virtual uint8_t writeRTC(const uint8_t addr, const uint8_t* values, const uint8_t nBytes) = 0;
         virtual uint8_t writeRTC(const uint8_t addr, const uint8_t value) = 0;
         virtual uint8_t readRTC(const uint8_t addr, uint8_t* values, const uint8_t nBytes) = 0;
         virtual uint8_t readRTC(const uint8_t addr) = 0;
+        virtual int16_t temperature() {return 0;};
 
     protected:
         TwoWire& wire;      // reference to Wire, Wire1, etc.
